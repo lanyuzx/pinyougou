1612e9bc60acee387b65b9055a96b5ca65c4fc5b
lanyuzx <lanyuzx@qq.com>
2019-07-26 11:19:17 +0800

    新增商品插入基本数据

--- pinyougou-shop-web/src/main/java/cn/lanlan/core/controller/LLGoodsController.java
+++ pinyougou-shop-web/src/main/java/cn/lanlan/core/controller/LLGoodsController.java
@@ -4,6 +4,8 @@ import cn.lanlan.core.pojo.entity.LLResult;
 import cn.lanlan.core.pojo.pojogroup.LLGoodsGroup;
 import cn.lanlan.core.service.LLGoodsService;
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -11,22 +13,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Type;
+
 @RestController
 @RequestMapping("/goods")
 public class LLGoodsController {
     @Reference
     private LLGoodsService goodsService;
     @RequestMapping("/add")
-    public LLResult addGoods(@RequestBody LLGoodsGroup goodsGroup) {
+    public LLResult addGoods( @RequestBody LLGoodsGroup goodsGroup) {
         //获取登录的商家id
       String sellerId =  SecurityContextHolder.getContext().getAuthentication().getName();
-      goodsGroup.getGoods().setSellerId(sellerId);
-      try {
-          goodsService.addGoods(goodsGroup);
-          return new LLResult(true, "添加成功");
-      }catch (Exception e) {
-          e.printStackTrace();
-          return new LLResult(false, "添加失败");
-      }
+        goodsGroup.getGoods().setSellerId(sellerId);
+        try {
+            goodsService.addGoods(goodsGroup);
+            return new LLResult(true, "添加成功");
+        }catch (Exception e) {
+            e.printStackTrace();
+            return new LLResult(false, "添加失败");
+        }
+
     }
 }
